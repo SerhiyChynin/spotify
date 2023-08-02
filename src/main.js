8ff6614c643a3570f5e597c76b765ac348babdfc
SerhiiChynin <sergey.chinin@gmail.com>
2023-08-02 16:45:59 +0300

    module controller

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-
-const start = async () => {
-    try {
-        const app = await NestFactory.create();
-    } catch (e) {
-        console.log(e);
- }
-}
