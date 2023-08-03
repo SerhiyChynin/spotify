00d51cfbbe7aed44a7e691320055a38292164ae6
SerhiiChynin <sergey.chinin@gmail.com>
2023-08-03 10:08:01 +0300

    schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
 import { TrackModule } from "./track/track.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-    imports: [TrackModule]
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost/nest'),
+        TrackModule]
 })
 export class AppModule{}
