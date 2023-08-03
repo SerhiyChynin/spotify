b67c67536258862bc8eff495f63aa7667b980113
SerhiiChynin <sergey.chinin@gmail.com>
2023-08-03 11:17:23 +0300

    service

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { Track, TrackDocument } from "./schemas/track.schema";
+import { Comment, CommentDocument } from "./schemas/comment.schema";
+import { Model } from "mongoose";
+import { InjectModel } from '@nestjs/mongoose';
 
 
 @Injectable()
 
 export class TrackService{
+
+    constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+                @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+    
+    ) { }
 
     async create() {
         
