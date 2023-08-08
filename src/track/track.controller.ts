a242a159b99796fed9331cb55cd8aecdf1604127
SerhiiChynin <sergey.chinin@gmail.com>
2023-08-08 17:37:48 +0300

    search

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller,Get, Post, Param , Delete, UseInterceptors, UploadedFiles } from "@nestjs/common";
+import { Body, Controller,Get, Post, Param , Delete, UseInterceptors, UploadedFiles, Query } from "@nestjs/common";
 import { TrackService } from "./track.service";
 import { CreateTrackDto } from "./dto/create-track.dto";
 import { ObjectId } from "mongoose";
@@ -21,8 +21,13 @@ export class TrackController {
       return this.trackService.create(dto, picture[0], audio[0]);    
     }
      @Get()
-     getAll() {
-        return this.trackService.getAll()
+     getAll(@Query('count') count: number,
+            @Query('offset') offset: number) {
+        return this.trackService.getAll(count, offset)
+    }
+     @Get('/search')
+     search(@Query('query') query: string) {
+        return this.trackService.search(query)
     }
     @Get(":id")
      getOne(@Param("id")id: ObjectId) {
